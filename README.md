fd94d0d0a62e624fd43a64d50e62044d8d96597d
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-06-09 13:34:59 +0600

    single page application done

---
+++ README.md
@@ -0,0 +1,15 @@
+# Edit User Modal Page
+
+## To run this code-
+
+    - Download this Repository
+    - Unzip the the file
+    - Open the index.html file with a browser
+
+## Technology used -
+
+    - JavaScript
+    - Bootstrap
+    - Font Awesome
+    - HTML
+    - CSS
---
+++ app.js
@@ -0,0 +1,16 @@
+const table = document.getElementById('user-table')
+table.addEventListener('click', e => {
+    const firstParent = e.target.parentNode
+    if (firstParent.className === 'edit-btn') {
+        const name = firstParent.parentNode.parentNode.children[0].textContent
+        const email = firstParent.parentNode.parentNode.children[1].textContent
+        const phone = firstParent.parentNode.parentNode.children[2].textContent
+        const role = firstParent.parentNode.parentNode.children[6].textContent
+        document.getElementById('nameField').value = name
+        document.getElementById('emailField').value = email
+        document.getElementById('phone').value = phone
+        document.getElementById('role').value = role
+    } else {
+        console.log('other clicked')
+    }
+})
