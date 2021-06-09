29e0c6b0c98cb82467c5c048743acde1a74babba
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-06-09 14:39:19 +0600

    code DRYied

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const table = document.getElementById('user-table')
 table.addEventListener('click', e => {
     const firstParent = e.target.parentNode
     if (firstParent.className === 'edit-btn') {
-        const name = firstParent.parentNode.parentNode.children[0].textContent
-        const email = firstParent.parentNode.parentNode.children[1].textContent
-        const phone = firstParent.parentNode.parentNode.children[2].textContent
-        const role = firstParent.parentNode.parentNode.children[6].textContent
+        const childList = firstParent.parentNode.parentNode.children
+        const name = childList[0].textContent
+        const email = childList[1].textContent
+        const phone = childList[2].textContent
+        const role = childList[6].textContent
         document.getElementById('nameField').value = name
         document.getElementById('emailField').value = email
         document.getElementById('phone').value = phone
